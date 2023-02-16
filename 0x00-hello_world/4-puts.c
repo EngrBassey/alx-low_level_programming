8bafdd36798b923d58724d02f9acb040c764958a
EngrBassey <basseyleads001@gmail.com>
2023-02-16 14:34:24 -0800

    C program that prints with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void) /* main - Entry point */
+{
+	char MyStr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(MyStr);
+	return (0);
+}
