1bbdf12921a2671579639f6c612cddc7a6a2b26f
EngrBassey <basseyleads001@gmail.com>
2023-03-21 06:02:22 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Structures, typedef projec tasks
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - object to create variable structure
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
