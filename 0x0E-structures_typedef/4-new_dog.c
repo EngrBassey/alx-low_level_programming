317f05917f00f683044ce6caf1dc150d416ff91d
EngrBassey <basseyleads001@gmail.com>
2023-03-21 10:09:10 -0700

    function that creates a new dog and allocate malloc memory

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,81 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+  * _strlen - funtion to print lenght of a string
+  * @str: string
+  * Return: return len;
+  */
+
+int _strlen(char *str)
+{
+	int i, len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+
+	return (len);
+}
+
+/**
+  * _strcpy - funtion that copy strings
+  * @dest: variable to copy to
+  * @str: string to copy
+  * Return: return dest
+  */
+
+char *_strcpy(char *dest, char *str)
+{
+	int lent, i;
+
+	lent = _strlen(str);
+
+	for (i = 0; i < lent; i++)
+	{
+		dest[i] = str[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+  * new_dog - funtion to create new dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: dog owner
+  * Return: return dog_Ptr
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int lent1, lent2;
+	dog_t *dogPtr;
+
+	dogPtr = malloc(sizeof(dog_t));
+
+	if (dogPtr == NULL)
+	{
+		return (NULL);
+	}
+
+	lent1 = _strlen(name);
+	lent2 = _strlen(owner);
+
+	dogPtr->name = malloc(sizeof(lent1 + 1));
+	dogPtr->owner = malloc(sizeof(lent2 + 2));
+
+	if (dogPtr->name == NULL || dogPtr->owner == NULL)
+	{
+		free(dogPtr);
+		return (NULL);
+	}
+
+	_strcpy(dogPtr->name, name);
+	_strcpy(dogPtr->owner, owner);
+
+	dogPtr->age = age;
+
+	return (dogPtr);
+}
