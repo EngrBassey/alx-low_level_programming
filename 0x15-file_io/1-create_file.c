71174620c3630bea1e25d7b8fc8d6ca6abcb3cd1
EngrBassey <basseyleads001@gmail.com>
2023-04-14 11:47:06 -0700

    function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+  * _strlen - function to get string length
+  * @str: pointer pointing to the string
+  * Return: return i
+  */
+
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+/**
+  * create_file - function that create a file with permissions
+  * @filename: file name
+  * @text_content: terminationg string to write to the file
+  * Return: return 1 on successs
+  */
+int create_file(const char *filename, char *text_content)
+{
+
+	int fdes, wrt;
+
+	if (filename == NULL)
+		return (-1);
+
+	fdes = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+	if (fdes == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		wrt = write(fdes, text_content, _strlen(text_content));
+
+		if (wrt == -1)
+			return (-1);
+	}
+
+	close(fdes);
+	return (1);
+}
