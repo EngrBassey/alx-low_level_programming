121efbd267cd5ebe321faf59123fc5b1197d1374
EngrBassey <basseyleads001@gmail.com>
2023-06-20 06:51:20 -0700

    function that creates a new dog.

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,86 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * new_dog - function that create new struct
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner
+  * Return: return ptr
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+	int lent1, lent2;
+
+	lent1 = _strlen(name) + 1;
+	lent2 = _strlen(owner) + 1;
+
+	if (name == NULL || age < 0 || owner == NULL)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	ptr->name = malloc(sizeof(char) * lent1);
+	if (ptr->name == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr->owner = malloc(sizeof(char) * lent2);
+	if (ptr->owner == NULL)
+	{
+		free(ptr->name);
+		free(ptr);
+	}
+	ptr->name = _strcpy(ptr->name, name);
+	ptr->age = age;
+	ptr->owner = _strcpy(ptr->name, owner);
+
+	return (ptr);
+}
+
+/**
+ * _strcpy - Entry point
+ * @dest: first pointer
+ * @src: second pointer
+ * Return: return dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int x = 0;
+
+	while (*(src + i) != '\0')
+	{
+		i++;
+	}
+	for (; x < i; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+  * _strlen - funtion that return string lenght
+  * @str: the string
+  * Return: return i
+  */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,10 @@ struct dog
 	char *owner;
 };
 typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _strlen(char *str);
+char *_strcpy(char *dest, char *src);
 #endif
