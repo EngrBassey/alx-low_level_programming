ead7fa212e574eaa33d80e14389c5f1eda971969
EngrBassey <basseyleads001@gmail.com>
2023-07-04 15:23:16 -0700

    constructot function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
+#include "lists.h"
 
 /**
   * firsFun - function to exce in main
   * excecuted before main()
   */
 
-void firsFun(void) __attribute__((constructor)); 
-
-/**
-  * firsFun - the function to excecute
- */
-
 void firsFun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	char *str;
+
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -27,4 +27,5 @@ list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *s);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void firsFun(void) __attribute__((constructor));
 #endif
