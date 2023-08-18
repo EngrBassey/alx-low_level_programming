731321e8c69514525ea0e5133049e1703136eeaa
EngrBassey <basseyleads001>
2023-08-18 15:28:29 -0700

    sum up nodes

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns all the data (n) sum
+ * @head: the head
+ * Return: the sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+	}
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
