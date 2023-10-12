d3668d07d496e339a18b762cf4debb3787ddc28c
EngrBassey <basseyleads001@gmail.com>
2023-10-11 21:45:22 -0700

    delete element in hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,32 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - function that deletes a hash table
- * @ht: hash table to be deleted
+ * hash_table_delete - Destroys hash table and free memory
+ * @ht: Hash table struct to destroy
  *
- * Return: void
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i = 0;
-	hash_node_t *node, *tmp;
+	hash_table_t *head = ht;
+	hash_node_t *tmp, *node;
+	unsigned long int i;
 
-	if (ht == NULL)
+	for (i = 0; i < ht->size; i++)
 	{
-		return;
-	}
-	for (; i < ht->size; i++)
-	{
-		node = ht->array[i];
-		while (node)
+		if (ht->array[i] != NULL)
 		{
-			tmp = node;
-			node = node->next;
-			free(tmp->key);
-			free(tmp->value);
-			free(tmp);
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
 		}
 	}
-	free(ht->array);
-	free(ht);
+	free(head->array);
+	free(head);
 }
+
